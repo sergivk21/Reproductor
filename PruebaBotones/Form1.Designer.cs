a7f3e36fb5b0c81dd29e7cd9a4987af9075bad17
Sergio Coelho <sergivk1@hotmail.com>
2025-03-04 12:32:51 +0100

    Primer commit

---
+++ PruebaBotones/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace PruebaBotones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botonCarpeta1 = new botones.BotonCarpeta();
+            this.botonCerrar1 = new botones.BotonCerrar();
+            this.adelantar1 = new botones.Adelantar();
+            this.botones1 = new botones.Botones();
+            this.atrasar1 = new botones.Atrasar();
+            this.barraProgreso1 = new botones.BarraProgreso();
+            this.botonImagenCancion1 = new botones.BotonImagenCancion();
+            this.panelFondo1 = new botones.PanelFondo();
+            this.songLabelDescrip1 = new botones.SongLabelDescrip();
+            this.songNameLabel1 = new botones.SongNameLabel();
+            this.panelFondo1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // botonCarpeta1
+            // 
+            this.botonCarpeta1.ColorBorde = System.Drawing.Color.Transparent;
+            this.botonCarpeta1.ColorFondo = System.Drawing.Color.OrangeRed;
+            this.botonCarpeta1.ColorIcono = System.Drawing.Color.White;
+            this.botonCarpeta1.DireccionIcono = true;
+            this.botonCarpeta1.FlatAppearance.BorderSize = 0;
+            this.botonCarpeta1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonCarpeta1.GrosorBorde = 2;
+            this.botonCarpeta1.Location = new System.Drawing.Point(12, 16);
+            this.botonCarpeta1.Name = "botonCarpeta1";
+            this.botonCarpeta1.Size = new System.Drawing.Size(50, 50);
+            this.botonCarpeta1.TabIndex = 2;
+            this.botonCarpeta1.Text = "botonCarpeta1";
+            this.botonCarpeta1.UseVisualStyleBackColor = true;
+            // 
+            // botonCerrar1
+            // 
+            this.botonCerrar1.ColorBorde = System.Drawing.Color.Transparent;
+            this.botonCerrar1.ColorFondo = System.Drawing.Color.Red;
+            this.botonCerrar1.ColorIcono = System.Drawing.Color.White;
+            this.botonCerrar1.FlatAppearance.BorderSize = 0;
+            this.botonCerrar1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonCerrar1.GrosorBorde = 2;
+            this.botonCerrar1.GrosorX = 4;
+            this.botonCerrar1.Location = new System.Drawing.Point(212, 16);
+            this.botonCerrar1.Name = "botonCerrar1";
+            this.botonCerrar1.Size = new System.Drawing.Size(50, 50);
+            this.botonCerrar1.TabIndex = 1;
+            this.botonCerrar1.Text = "botonCerrar1";
+            this.botonCerrar1.UseVisualStyleBackColor = true;
+            // 
+            // adelantar1
+            // 
+            this.adelantar1.ColorBorde = System.Drawing.Color.Transparent;
+            this.adelantar1.ColorFondo = System.Drawing.Color.Red;
+            this.adelantar1.ColorIcono = System.Drawing.Color.White;
+            this.adelantar1.DireccionIcono = true;
+            this.adelantar1.FlatAppearance.BorderSize = 0;
+            this.adelantar1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.adelantar1.GrosorBorde = 2;
+            this.adelantar1.Location = new System.Drawing.Point(184, 378);
+            this.adelantar1.Name = "adelantar1";
+            this.adelantar1.Size = new System.Drawing.Size(50, 50);
+            this.adelantar1.TabIndex = 0;
+            this.adelantar1.Text = "adelantar1";
+            this.adelantar1.UseVisualStyleBackColor = true;
+            // 
+            // botones1
+            // 
+            this.botones1.ColorBorde = System.Drawing.Color.Transparent;
+            this.botones1.ColorFondo = System.Drawing.Color.Green;
+            this.botones1.ColorIcono = System.Drawing.Color.White;
+            this.botones1.FlatAppearance.BorderSize = 0;
+            this.botones1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botones1.GrosorBorde = 2;
+            this.botones1.Location = new System.Drawing.Point(111, 378);
+            this.botones1.Name = "botones1";
+            this.botones1.Size = new System.Drawing.Size(50, 50);
+            this.botones1.TabIndex = 3;
+            this.botones1.Text = "botones1";
+            this.botones1.UseVisualStyleBackColor = true;
+            // 
+            // atrasar1
+            // 
+            this.atrasar1.ColorBorde = System.Drawing.Color.Transparent;
+            this.atrasar1.ColorFondo = System.Drawing.Color.Red;
+            this.atrasar1.ColorIcono = System.Drawing.Color.White;
+            this.atrasar1.DireccionIcono = false;
+            this.atrasar1.FlatAppearance.BorderSize = 0;
+            this.atrasar1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.atrasar1.GrosorBorde = 2;
+            this.atrasar1.Location = new System.Drawing.Point(39, 378);
+            this.atrasar1.Name = "atrasar1";
+            this.atrasar1.Size = new System.Drawing.Size(50, 50);
+            this.atrasar1.TabIndex = 4;
+            this.atrasar1.Text = "atrasar1";
+            this.atrasar1.UseVisualStyleBackColor = true;
+            // 
+            // barraProgreso1
+            // 
+            this.barraProgreso1.ColorBarra = System.Drawing.Color.OrangeRed;
+            this.barraProgreso1.ColorDeslizador = System.Drawing.Color.OrangeRed;
+            this.barraProgreso1.ColorTexto = System.Drawing.Color.White;
+            this.barraProgreso1.Location = new System.Drawing.Point(12, 332);
+            this.barraProgreso1.Maximo = 100;
+            this.barraProgreso1.Minimo = 0;
+            this.barraProgreso1.Name = "barraProgreso1";
+            this.barraProgreso1.Size = new System.Drawing.Size(250, 30);
+            this.barraProgreso1.TabIndex = 5;
+            this.barraProgreso1.Text = "barraProgreso1";
+            this.barraProgreso1.Valor = 0;
+            // 
+            // botonImagenCancion1
+            // 
+            this.botonImagenCancion1.ImagenCancion = null;
+            this.botonImagenCancion1.Location = new System.Drawing.Point(26, 54);
+            this.botonImagenCancion1.Name = "botonImagenCancion1";
+            this.botonImagenCancion1.Size = new System.Drawing.Size(224, 232);
+            this.botonImagenCancion1.TabIndex = 6;
+            this.botonImagenCancion1.Text = "botonImagenCancion1";
+            // 
+            // panelFondo1
+            // 
+            this.panelFondo1.BackColor = System.Drawing.Color.Black;
+            this.panelFondo1.Controls.Add(this.botonCerrar1);
+            this.panelFondo1.Controls.Add(this.botonCarpeta1);
+            this.panelFondo1.Controls.Add(this.songNameLabel1);
+            this.panelFondo1.Controls.Add(this.songLabelDescrip1);
+            this.panelFondo1.Controls.Add(this.botonImagenCancion1);
+            this.panelFondo1.Controls.Add(this.barraProgreso1);
+            this.panelFondo1.Controls.Add(this.botones1);
+            this.panelFondo1.Controls.Add(this.atrasar1);
+            this.panelFondo1.Controls.Add(this.adelantar1);
+            this.panelFondo1.Location = new System.Drawing.Point(225, 0);
+            this.panelFondo1.Name = "panelFondo1";
+            this.panelFondo1.Size = new System.Drawing.Size(278, 449);
+            this.panelFondo1.TabIndex = 7;
+            // 
+            // songLabelDescrip1
+            // 
+            this.songLabelDescrip1.AutoSize = true;
+            this.songLabelDescrip1.BackColor = System.Drawing.Color.Transparent;
+            this.songLabelDescrip1.Font = new System.Drawing.Font("Arial", 12F);
+            this.songLabelDescrip1.ForeColor = System.Drawing.Color.White;
+            this.songLabelDescrip1.Location = new System.Drawing.Point(89, 289);
+            this.songLabelDescrip1.Name = "songLabelDescrip1";
+            this.songLabelDescrip1.Size = new System.Drawing.Size(95, 18);
+            this.songLabelDescrip1.TabIndex = 7;
+            this.songLabelDescrip1.Text = "Sam Fender";
+            this.songLabelDescrip1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // songNameLabel1
+            // 
+            this.songNameLabel1.AutoSize = true;
+            this.songNameLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.songNameLabel1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+            this.songNameLabel1.ForeColor = System.Drawing.Color.White;
+            this.songNameLabel1.Location = new System.Drawing.Point(78, 257);
+            this.songNameLabel1.Name = "songNameLabel1";
+            this.songNameLabel1.Size = new System.Drawing.Size(118, 29);
+            this.songNameLabel1.TabIndex = 8;
+            this.songNameLabel1.Text = "Play God";
+            this.songNameLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelFondo1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelFondo1.ResumeLayout(false);
+            this.panelFondo1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private botones.Adelantar adelantar1;
+        private botones.BotonCerrar botonCerrar1;
+        private botones.BotonCarpeta botonCarpeta1;
+        private botones.Botones botones1;
+        private botones.Atrasar atrasar1;
+        private botones.BarraProgreso barraProgreso1;
+        private botones.BotonImagenCancion botonImagenCancion1;
+        private botones.PanelFondo panelFondo1;
+        private botones.SongNameLabel songNameLabel1;
+        private botones.SongLabelDescrip songLabelDescrip1;
+    }
+}
+
